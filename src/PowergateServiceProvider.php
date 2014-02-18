de720c9c0bf48f0385b5a555775d7d6515a93337
Bobby Allen <ballen@bobbyallen.me>
2014-02-18 20:50:21 +0000

    Added updates to the PowergateServiceProvider in order to pass through the baseUrl, API username and key from a config file.

--- src/PowergateServiceProvider.php
+++ src/PowergateServiceProvider.php
@@ -1,14 +1,21 @@
 <?php
 
 use Illumate\Support\ServiceProvider;
+use Ballen\PowergateClient\Domain;
+use Ballen\PowergateClient\Record;
+use Config;
 
 class PowergateServiceProvider extends ServiceProvider
 {
 
     public function register()
     {
-        $this->app->bind('domain', 'Ballen\PowergateClient\Domain');
-        $this->app->bind('record', 'Ballen\PowergateClient\Record');
+        $this->app->bind('domain', function() {
+            return new Domain(Config::get('powergate.api.baseUrl', ''), Config::get('powergate.api.user', 'api'), Config::get('powergate.api.key', 'no_password_set'));
+        });
+        $this->app->bind('record', function() {
+            return new Record(Config::get('powergate.api.baseUrl', ''), Config::get('powergate.api.user', 'api'), Config::get('powergate.api.key', 'no_password_set'));
+        });
     }
 
 }
